40c16786d39384a6cc81b9087c8f8399186b35f1
asenterziyski <asenterziyski@gmail.com>
2025-10-11 09:30:50 +0300

    HydroCalc 16 refactor navigation: use sealed class instead of object

--- app/src/main/java/com/example/hydrocalculator/MainActivity.kt
+++ app/src/main/java/com/example/hydrocalculator/MainActivity.kt
@@ -6,24 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.hydrocalculator.navigation.AppRoutes
+import com.example.hydrocalculator.navigation.HydroAppRoutes
 import com.example.hydrocalculator.ui.theme.HydrocalculatorTheme
 import com.example.hydrocalculator.views.MainScreen
 import com.example.hydrocalculator.views.WelcomeScreen
@@ -49,20 +40,20 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
-                    startDestination = AppRoutes.WELCOME_SCREEN
+                    startDestination = HydroAppRoutes.Welcome.route
                 ) {
 
-                    composable(AppRoutes.WELCOME_SCREEN) {
+                    composable(route = HydroAppRoutes.Welcome.route) {
                         WelcomeScreen(
                             onWelcomeComplete = {
-                                navController.navigate(AppRoutes.MAIN_SCREEN) {
-                                    popUpTo(AppRoutes.WELCOME_SCREEN) { inclusive = true }
+                                navController.navigate(route = HydroAppRoutes.Main.route) {
+                                    popUpTo(HydroAppRoutes.Welcome.route) { inclusive = true }
                                 }
                             }
                         )
                     }
 
-                    composable(AppRoutes.MAIN_SCREEN) {
+                    composable(route = HydroAppRoutes.Main.route) {
                         AppScaffold(
                             title = "Main Screen"
                         ) { modifier -> MainScreen(modifier = modifier) }
--- app/src/main/java/com/example/hydrocalculator/navigation/HydroAppNavigation.kt
+++ app/src/main/java/com/example/hydrocalculator/navigation/HydroAppNavigation.kt
@@ -1,6 +1,6 @@
 package com.example.hydrocalculator.navigation
 
-object AppRoutes {
-    const val WELCOME_SCREEN = "welcome_screen"
-    const val MAIN_SCREEN = "main_screen"
+sealed class HydroAppRoutes(val route: String) {
+    object Welcome: HydroAppRoutes("welcome_screen")
+    object Main: HydroAppRoutes("Main_screen")
 }
