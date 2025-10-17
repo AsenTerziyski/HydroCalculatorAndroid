34823be12729a662d4dcd65da5cb394cbc9cf378
asenterziyski <asenterziyski@gmail.com>
2025-10-17 11:57:16 +0300

    HydroCalc 25 fix redeclaration error

---
+++ app/src/main/java/com/example/hydrocalculator/HydroAppBar.kt
@@ -0,0 +1,64 @@
+package com.example.hydrocalculator
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.sp
+import com.example.hydrocalculator.ui.theme.hydroGradient
+import kotlinx.coroutines.delay
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HydroAppBar(title: String = "") {
+
+    val fullTitle = title
+    var visibleTitle by remember { mutableStateOf("") }
+    var startAnimation by remember { mutableStateOf(false) }
+
+    val minFontSize = 7.sp
+    val maxFontSize = MaterialTheme.typography.titleLarge.fontSize
+
+    val animatedFontSize by animateFloatAsState(
+        targetValue = if (startAnimation) maxFontSize.value else minFontSize.value,
+        label = "fontSizeAnimation",
+        animationSpec = androidx.compose.animation.core.tween(durationMillis = fullTitle.length * 100)
+    )
+
+    // 3. This effect handles the letter-by-letter reveal
+    LaunchedEffect(fullTitle) {
+        if (fullTitle.isNotEmpty()) {
+            visibleTitle = ""
+            startAnimation = false
+            delay(100)
+            startAnimation = true
+            fullTitle.forEachIndexed { index, _ ->
+                visibleTitle = fullTitle.substring(0, index + 1)
+                delay(20)
+            }
+        }
+    }
+
+    TopAppBar(
+        title = {
+            Text(
+                text = visibleTitle,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    brush = MaterialTheme.hydroGradient,
+                    fontSize = animatedFontSize.sp
+                )
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    )
+}
---
+++ app/src/main/java/com/example/hydrocalculator/views/LoadingView.kt
@@ -0,0 +1,55 @@
+package com.example.hydrocalculator.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+
+@Composable
+fun LoadingView(
+    modifier: Modifier = Modifier,
+    text: String = "Loading",
+    color: Color = Color.White
+) {
+    var visibleDotCount by remember { mutableIntStateOf(0) }
+    LaunchedEffect(Unit) {
+        while (true) {
+            visibleDotCount = (visibleDotCount + 1) % 4
+            delay(700)
+        }
+    }
+
+    val annotatedString = buildAnnotatedString {
+        append(text)
+        for (i in 1..3) {
+            val dotColor = if (i <= visibleDotCount) color else Color.Transparent
+            withStyle(style = SpanStyle(color = dotColor)) {
+                append(".")
+            }
+        }
+    }
+
+    Text(
+        text = annotatedString,
+        modifier = modifier,
+        textAlign = TextAlign.Center
+    )
+
+}
+
+@Preview
+@Composable
+fun LoadingViewPreview() {
+    LoadingView()
+}
