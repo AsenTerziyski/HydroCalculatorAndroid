7a36d8351be7a2af9716f23583e1ccb0b734aee9
asenterziyski <asenterziyski@gmail.com>
2025-11-18 10:24:12 +0200

    HydroCalc 54 calculate flow speed

--- app/src/main/java/com/example/hydrocalculator/calculationengine/PressurePipeEngine.kt
+++ app/src/main/java/com/example/hydrocalculator/calculationengine/PressurePipeEngine.kt
@@ -2,5 +2,11 @@ package com.example.hydrocalculator.calculationengine
 
 object PressurePipeEngine {
     fun estimateVelocity(flow: Float, diameter: Float): Float =
-        (flow / 1000) / (diameter / 1000)
+        (flow / 1000) / getCrossSectionArea(diameter)
+
+    private fun getCrossSectionArea(diameter: Float): Float {
+        val diameterInMeters = diameter / 1000
+        return Math.PI.toFloat() * (diameterInMeters / 2) * (diameterInMeters / 2)
+    }
+
 }
