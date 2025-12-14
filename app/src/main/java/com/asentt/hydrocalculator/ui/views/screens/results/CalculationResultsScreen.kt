a29e95feb9a37a210e1f3323f9f5e3d85b2fd088
asenterziyski <asenterziyski@gmail.com>
2025-12-14 10:51:22 +0200

    HydroCalc 85 fetch results in Results screen

--- app/src/main/java/com/asentt/hydrocalculator/ui/views/screens/results/CalculationResultsScreen.kt
+++ app/src/main/java/com/asentt/hydrocalculator/ui/views/screens/results/CalculationResultsScreen.kt
@@ -1,26 +1,43 @@
 package com.asentt.hydrocalculator.ui.views.screens.results
 
-
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.asentt.hydrocalculator.ui.views.LoadingView
+import com.asentt.hydrocalculator.utils.Resource
 
 @Composable
 fun CalculationResultsScreen(
+    viewModel: ResultsViewModel = hiltViewModel()
 ) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Calculation results will be displayed here.",
-            style = MaterialTheme.typography.titleMedium
-        )
+        val resultState = viewModel.resultHistoryState.collectAsStateWithLifecycle()
+        val results = resultState.value
+
+        when(results) {
+            Resource.Loading -> {
+                LoadingView()
+            }
+            is Resource.Error -> {
+
+            }
+            is Resource.Success<*> -> {
+                val results = results.data as? List<*>
+                results?.forEach { result ->
+                    Log.d("TAG101", "Result: $result")
+                }
+            }
+            else -> {}
+        }
     }
 }
 
