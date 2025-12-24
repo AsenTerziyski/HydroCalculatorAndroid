610b48d96e2ae022ecbb72f858d856f49d29bb00
asenterziyski <asenterziyski@gmail.com>
2025-12-24 17:42:53 +0200

    HydroCalc 97 update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // In your TOP-LEVEL build.gradle.kts file
 
 plugins {
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false // <-- APPLY THIS CHANGE
